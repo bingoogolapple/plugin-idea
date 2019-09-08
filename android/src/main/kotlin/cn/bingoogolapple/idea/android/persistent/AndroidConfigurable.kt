5c7daa818b1796295a3681fd313ca69bb07ebb7d
bingoogolapple <bingoogolapple@gmail.com>
2019-09-08 12:33:45 +0800

    添加注释

--- android/src/main/kotlin/cn/bingoogolapple/idea/android/persistent/AndroidConfigurable.kt
+++ android/src/main/kotlin/cn/bingoogolapple/idea/android/persistent/AndroidConfigurable.kt
@@ -10,22 +10,27 @@ class AndroidConfigurable : Configurable {
     private var configForm = AndroidConfigForm()
     private val persistent = ServiceManager.getService(AndroidPersistentStateComponent::class.java)
 
+    // 控制在配置面板中左侧窗口的显示名称
     override fun getDisplayName(): String {
         return Util.getIdentity("")
     }
 
+    // 配置界面的绘制，负责用户输入信息的接受
     override fun createComponent(): JComponent? {
         return configForm.contentView
     }
 
+    // 当用户修改配置参数后，在点击 OK、Apply 按钮前，框架会自动调用该方法，判断是否有修改，进而控制 OK、Apply 按钮是否可用
     override fun isModified(): Boolean {
         return configForm.state != persistent.state
     }
 
+    // 用户点击 OK、Apply 按钮后会调用该方法
     override fun apply() {
         persistent.loadState(configForm.state)
     }
 
+    // 用户点击 Reset 按钮后会调用该方法，用户界面配置参数重置
     override fun reset() {
         configForm.reset(persistent.state)
     }
